e51ef456a4549d948149c1adae7fee4ff8d6b620
Mohammad jaradat <morjaradat66@gmail.com>
2021-05-17 21:35:27 +0300

    add booklibrary

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel'
+import codebook from './asstes/image/howToCode.jpg'
 
 class MyFavoriteBooks extends React.Component {
+  async componentDidMount() {
+    try {
+      const booksUrl = `${process.env.REACT_APP_MONGODB}?email=${this.props.auth0.user.email}`;
+      // console.log(this.props.auth0.user.email)
+      let booksData = await axios.get(booksUrl)
+      this.props.update(booksData)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   render() {
-    return(
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
+    return (
+      <div className='cardcontainer'>
+        <Carousel>
+          {this.props.data.map(i => {
+            return (
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={codebook}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>Book Name :{i.name}</h3>
+                  <p>description :{i.description}</p>
+                  <p>status :{i.status}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            )
+          })}
+        </Carousel>
+      </div>
     )
   }
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -5,12 +5,11 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 function Profile() {
   const { user } = useAuth0();
-  console.log(user)
+  // console.log(user)
   return (
+    // this.props.bookslibary(user.email),
     <>
-      {/* <div>Hello {user.name}</div>
-      <div>Hello {user.email}</div>
-      <img src={user.picture} alt='' /> */}
+      
       <Card style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>Hello {user.name}</Card.Title>
